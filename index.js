46d15205d473bcc7ffe0191258c659e544c99f37
Moollihawkja <jared@wisen.space>
2021-11-27 19:52:14 -0800

    added Procfile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
  
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -13,4 +14,4 @@ app.get('/toothfairy', function (req, res) {
     res.send('I will give you money for your tooth')
 })
  
-app.listen(3000)
+app.listen(PORT)
