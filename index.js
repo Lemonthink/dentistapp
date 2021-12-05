6cec482826c45d65fd6339a9cef5ae75729162b2
Andrew Peng <andrew.w.peng@gmail.com>
2021-12-04 20:25:37 -0800

    Added input list

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ const app = express()
 const PORT = process.env.PORT || 3000
  
 app.get('/', function (req, res) {
-  res.sendFile(__dirname+'/homepage.html');
+	res.sendFile(__dirname + '/homepage.html');
 })
 
 app.get('/miro', function (req, res) {
-  res.redirect('https://miro.com/app/board/o9J_llS_z48=/');
+	res.redirect('https://miro.com/app/board/o9J_llS_z48=/');
 })
 
 app.get('/dentist', function (req, res) {
     res.send('I will find a treatment you')
 })
+
+app.get('/data', function (req, res) {
+	//insert dataset using res.redirect or something in csv file
+})
+
 app.get('/readme', function (req, res) {
   res.sendFile(__dirname+'/README.md')
 })
+
 app.get('/toothfairy', function (req, res) {
     res.send('I will give you money for your tooth')
 })
- 
+
 app.listen(PORT)
